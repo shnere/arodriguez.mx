98ab8036162983ffd44697941701b1a0be0fd5d5
shnere <alanrodriguezromero@gmail.com>
2015-07-20 23:00:15 -0500

    Gulp watch

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('styles/*.scss')
+    gulp.src('styles/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('styles'));
+});
+
+gulp.task('default',function() {
+    gulp.watch('styles/style.scss',['styles']);
 });
